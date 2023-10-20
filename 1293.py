bab519a18964f76da9a69bc3797f553203f0c425
Alex Kang <63813437+alexhanbich@users.noreply.github.com>
2023-10-19 23:06:06 -0600

    problem 1293

---
+++ 1293.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
+# 1293. Shortest Path in a Grid with Obstacles Elimination
+
+from collections import deque
+
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        num_rows = len(grid)
+        num_cols = len(grid[0])
+
+        visited = {}
+        queue = deque()
+        root = (0, 0, 0, k)
+        queue.append(root)
+        while len(queue):
+            node = queue.popleft()
+            i, j, steps, k_i = node
+            if i == num_rows-1 and j == num_cols-1: # success
+                return steps
+            if i < 0 or j < 0 or i >= num_rows or j >= num_cols: # out of bounds
+                continue
+            if (i, j) in visited and k_i <= visited[(i,j)]: # not a better path
+                continue
+            visited[(i,j)] = k_i # add to visited
+            if grid[i][j] == 1: # if obstacle, use k
+                k_i -= 1
+                if k_i < 0: # if don't have k, can't continue path
+                    continue
+            
+            steps += 1
+            queue.append((i-1, j, steps, k_i))
+            queue.append((i+1, j, steps, k_i))
+            queue.append((i, j-1, steps, k_i))
+            queue.append((i, j+1, steps, k_i))
+        return -1

1714ae58cf32ee5aa919d50c06208a6c29146a3d
Alex Kang <63813437+alexhanbich@users.noreply.github.com>
2023-10-19 23:29:58 -0600

    add comments

--- 1293.py
+++ 1293.py
@@ -1,9 +1,30 @@
 # https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
 # 1293. Shortest Path in a Grid with Obstacles Elimination
 
+# Intuition:
+# - finding shortest path -> use BFS
+# - if there are multiple paths to the same cell, use path with higher k value, 
+#   and to do that, k must be tracked.
+# - pass steps so that it can be returned
+
+# Stop exploring condition:
+# - if out of bounds, don't explore
+# - if not enough k, don't explore
+# - if a previous path is better (has higher k), don't explore
+
+# Time Complexity:
+# - need to traverse the grid (M*N)
+# - can explore the same cell up to k times (k)
+# Total: O(M*N*k)
+
+# Space Complexity:
+# - need to store all cells (M*N)
+# - lengh of queue is at worst holding all cells (M*N)
+# Total: O(M*N)
+
 from collections import deque
 
-def shortestPath(grid, k):
+def shortest_path(grid, k):
     num_rows = len(grid)
     num_cols = len(grid[0])
 
